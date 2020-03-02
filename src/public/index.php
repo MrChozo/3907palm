22f724b3f247b0b5a764286d8e76c2081acfd6fb
Brian Weston <brianw@webriculture.com>
2020-03-01 22:00:13 -0800

    Dunno, too tired

--- src/public/index.php
+++ src/public/index.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>3907 Palm Boulevard</title>
+  <title>3907 Palm Boulevard</title>
+  <link rel="stylesheet" type="text/css" href="/style.css">
 </head>
 <body>
-	<style>
-		header {
-
-		}
-		nav {
-		}
-		jumbotron {
-			background: red;
-		}
-		main {
-			background: blue;
-		}
-		aside {
-
-		}
-		footer {
-			background: green;
-		}
-
-	</style>
-	<div id="app">
-		<header>
-			<nav></nav>
-		</header>
-		<jumbotron></jumbotron>
-		<main></main>
-		<aside></aside>
-		<footer></footer>
-	</div>	
+  <div id="app">
+    <header>
+      <nav></nav>
+    </header>
+    <div id="full-wide">
+      <div id="jumbotron">
+        <h1>FOIEntawoifetphnoIFWEkto"IWFUEkyWFu</h1>
+        <img src="/img/23-Left Side 40FT-MLS.jpg" alt="Looking down on the property">
+      </div>
+      <div id="carousel">
+        <img src="/img/20190717_131031_1583027507260.jpg">
+        <img src="/img/20190717_131201_1583027505470.jpg">
+        <img src="/img/20190717_131702_001_1583027506342.jpg">
+        <img src="/img/hero.jfif">
+        <img src="/img/PALM-BLVD-3907fi-2.jpg">
+        <img src="/img/0f1c41c5-c8e5-4db4-b93a-cb7908c867f9.f10.webp">
+        <img src="/img/1-High Aerial Shots-MLS.jpg">
+        <img src="/img/2-High Aerial Shots-MLS.jpg">
+        <img src="/img/20190717_130950_1583027510959.jpg">
+        <img src="/img/20190717_131012_1583027511605.jpg">
+      </div>
+      <main>
+        
+      </main>
+      <aside></aside>
+    </div>
+    <footer></footer>
+  </div>  
 </body>
 </html>
