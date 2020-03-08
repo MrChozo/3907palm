bf512249f0c63767cc30d6c46ab6ed9d0b988044
Brian Weston <brianw@webriculture.com>
2020-03-07 19:25:23 -0800

    Scratch JS; Simplify to one image

--- src/public/html/home.php
+++ src/public/html/home.php
@@ -1,69 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title><?= $site_title ?></title>
+  <meta charset="UTF-8">
+  <title><?= $site_title ?></title>
 
-    <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <header>
-    <nav></nav>
+  <nav></nav>
 </header>
 <main>
-    <section id="jumbotron">
-        <div>
-            <a href="https://www.zillow.com/homes/3907-Palm-Boulevard-Isle-of-Palms,-SC-29451_rb/10937177_zpid/">
-                <h1>3907 Palm Boulevard</h1>
-                <h1>Isle of Palms, SC 29451</h1>
-                <h1>$1,149,900</h1>
-            </a>
-        </div>
-    </section>
-    <section id="carousel">
-        <img src="../img/20190717_131031_1583027507260.jpg" alt="">
-        <img src="../img/20190717_131201_1583027505470.jpg" alt="">
-        <img src="../img/20190717_131702_001_1583027506342.jpg" alt="">
-    </section>
-    <section id="small-images">
-        <img src="../img/0f1c41c5-c8e5-4db4-b93a-cb7908c867f9.f10.webp" alt="">
-        <img src="../img/1-High%20Aerial%20Shots-MLS.jpg" alt="">
-        <img src="../img/2-High%20Aerial%20Shots-MLS.jpg" alt="">
-        <img src="../img/20190717_130950_1583027510959.jpg" alt="">
-        <img src="../img/20190717_131012_1583027511605.jpg" alt="">
-    </section>
-    <section id="list-image-left">
-        <img src="../img/PALM-BLVD-3907fi-2.jpg" alt="">
-        <ul>
-            <li>Easily add an addition, or rent now until you build your dream home. Reduced and priced to sell!</li>
-            <li>2 BR, 2 BA; 1194 sq. ft.</li>
-            <li>Directly across from beach access path</li>
-            <li>Hexagonal shaped beach cottage with custom built cupola</li>
-            <li>Heart pine walls and ceilings</li>
-        </ul>
-    </section>
-    <section id="list-image-right">
-        <img src="../img/hero.jfif" alt="">
-        <ul>
-            <li>VE flood zone</li>
-            <li>.25 acres</li>
-            <li>Large lot with trees trimmed to provide most room for building</li>
-            <li>Second row from the beach</li>
-        </ul>
-    </section>
-    <section id="contact-form">
-        <h2>Contact Connie Gawrych for more info.</h2>
-        <!--
-          843-224-7278
-          cgawrych@gmail.com
+  <section id="jumbotron">
+    <div>
+      <a href="https://www.zillow.com/homes/3907-Palm-Boulevard-Isle-of-Palms,-SC-29451_rb/10937177_zpid/">
+        <h1>3907 Palm Boulevard</h1>
+        <h1>Isle of Palms, SC 29451</h1>
+        <h1>$1,149,900</h1>
+      </a>
+    </div>
+  </section>
+  <section id="list-no-image">
+    <article>
+      <h2>Contact Connie Gawrych for more info at <a href="mailto:cgawrych@gmail.com">cgawrych@gmail.com</a></h2>
+      <ul>
+        <li>Easily add an addition, or rent now until you build your dream home. Reduced and priced to sell!</li>
+        <li>2 BR, 2 BA; 1194 sq. ft.</li>
+        <li>Directly across from beach access path</li>
+        <li>Hexagonal shaped beach cottage with custom built cupola</li>
+        <li>Heart pine walls and ceilings</li>
+        <li>VE flood zone</li>
+        <li>.25 acres</li>
+        <li>Large lot with trees trimmed to provide most room for building</li>
+        <li>Second row from the beach</li>
+      </ul>
+    </article>
+  </section>
+  <!--
+    843-224-7278
 
-          Contact Form
-          Name
-          Email
-          Message
-        -->
-    </section>
+    Contact Form
+    Name
+    Email
+    Message
+  -->
 </main>
-<footer></footer>
 </body>
+<script>
+  // let elEyes = document.getElementsByTagName(li);
+  // elEyes.forEach(element => addContainer(element));
+  //
+  // function addContainer(element) {
+  //   let tempDiv = document.createElement('div').addClass('container');
+  //   element.insertAdjacentElement('beforebegin',;
+  //
+  // }
+</script>
 </html>
