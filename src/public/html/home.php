96e1904cb75fac06b0cc9b890afab3e5bb880d7b
Brian Weston <brianw@webriculture.com>
2020-03-08 20:39:57 -0700

    Stick in Cloudinary <picture>

--- src/public/html/home.php
+++ src/public/html/home.php
@@ -12,6 +12,43 @@
 </header>
 <main>
   <section id="jumbotron">
+    <picture>
+      <source
+          media="(max-width: 767px)"
+          sizes="(max-width: 767px) 100vw, 767px"
+          srcset="
+          23-Left_20Side_2040FT-MLS_n2cvur_ar_1_1,c_fill,g_auto__c_scale,w_200.jpg 200w,
+          23-Left_20Side_2040FT-MLS_n2cvur_ar_1_1,c_fill,g_auto__c_scale,w_475.jpg 475w,
+          23-Left_20Side_2040FT-MLS_n2cvur_ar_1_1,c_fill,g_auto__c_scale,w_641.jpg 641w,
+          23-Left_20Side_2040FT-MLS_n2cvur_ar_1_1,c_fill,g_auto__c_scale,w_767.jpg 767w">
+      <source
+          media="(min-width: 768px) and (max-width: 991px)"
+          sizes="(max-width: 991px) 70vw, 694px"
+          srcset="
+          23-Left_20Side_2040FT-MLS_n2cvur_ar_4_3,c_fill,g_auto__c_scale,w_538.jpg 538w,
+          23-Left_20Side_2040FT-MLS_n2cvur_ar_4_3,c_fill,g_auto__c_scale,w_694.jpg 694w">
+      <source
+          media="(min-width: 992px) and (max-width: 1199px)"
+          sizes="(max-width: 1200px) 60vw, 720px"
+          srcset="
+          23-Left_20Side_2040FT-MLS_n2cvur_ar_16_9,c_fill,g_auto__c_scale,w_596.jpg 596w,
+          23-Left_20Side_2040FT-MLS_n2cvur_ar_16_9,c_fill,g_auto__c_scale,w_720.jpg 720w">
+      <img
+          sizes="(max-width: 4800px) 40vw, 1920px"
+          srcset="
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_480.jpg 480w,
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_762.jpg 762w,
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_983.jpg 983w,
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_1175.jpg 1175w,
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_1347.jpg 1347w,
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_1503.jpg 1503w,
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_1658.jpg 1658w,
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_1791.jpg 1791w,
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_1919.jpg 1919w,
+          23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_1920.jpg 1920w"
+          src="23-Left_20Side_2040FT-MLS_n2cvur_c_scale,w_1920.jpg"
+          alt="Build height view of distance to beach from property">
+    </picture>
     <div>
       <a href="https://www.zillow.com/homes/3907-Palm-Boulevard-Isle-of-Palms,-SC-29451_rb/10937177_zpid/"
          target="_blank" rel="noreferrer noopener">
