5ff9d0f6d872c474b1f883591dc4571bd58899f0
Brian Weston <brianw@webriculture.com>
2020-04-11 11:50:02 -0700

    Google Analytics

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -6,15 +6,28 @@
     <title>{{ env('APP_NAME') }}</title>
 
     <link rel="stylesheet" href="{{ asset('css/style0.css') }}">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-F59XK8H3DN"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'G-F59XK8H3DN');
+  </script>
 </head>
 <body>
 <header>
-    <nav></nav>
+  <nav></nav>
 </header>
 <main>
 
-    @yield('content')
+  @yield('content')
 
 </main>
 </body>
